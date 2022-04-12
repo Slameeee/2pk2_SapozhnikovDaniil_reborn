fed8cbc877a7cc77e62909ea00a427c1a026e37a
1 <1>
2022-04-12 11:29:16 +0500

    Добавьте файлы проекта.

---
+++ pz_01/Atom.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pz_01
+{
+    /*
+     21 вариант
+Создать класс Atom описывающий характеристики химического вещества как сущность в 
+приложении
+Член класса название особенности
+поле name Имя хим элемента
+поле countElectrons Количество электроном
+поле mass Масса атома
+поле valence валентность
+метод GetInfo() выводит описание текущего объекта
+     */
+    class Atom
+    {
+        string name;
+        int countElectrons;
+        double mass;
+        int valence;
+
+        public Atom(string a, int b, double c, int d)
+        {
+            name = a;
+            countElectrons = b;
+            mass = c;
+            valence = d;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine("{0}", name);
+            Console.WriteLine("{0}", countElectrons);
+            Console.WriteLine("{0}", mass);
+            Console.WriteLine("{0}", valence);
+        }
+    }
+}
---
+++ pz_01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace pz_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Atom o2 = new Atom("imya",1,2,9);
+            Atom o3 = new Atom("familiya", 9, 4, 0);
+            o2.GetInfo();
+            o3.GetInfo();
+        }
+    }
+}
