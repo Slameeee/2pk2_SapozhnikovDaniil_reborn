6003f4c2ccc72eab9b0d5117608dc27a88fc1f26
Slameeee <93373372+Slameeee@users.noreply.github.com>
2022-04-20 18:59:57 +0500

    Add files via upload

---
+++ PZ9.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+delegate void MyDelegate();
+
+namespace pz_9
+{    class Account
+    {
+        public double money;
+
+        public void Plus(int a)
+        {
+            money += a;
+        }
+        public void Minus(int a)
+        {
+            money -= a;
+        }
+    }
+
+    class Client 
+    {
+        Account acc;
+
+        public event MyDelegate eventt;        
+
+        public Client(Account a)
+        {
+            acc = a;
+        }
+
+        public void HowMoney()
+        {
+            if (acc.money < 10)
+            {
+                eventt += Message;
+                eventt();                
+            }
+            if(acc.money >=10)
+            {
+                eventt-=Message;
+                Console.WriteLine("деньги есть");
+            }
+        }
+
+        private void Message()
+        {
+            Console.WriteLine("на вашем счету мало денег");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account();
+            Client client = new Client(account);
+
+            Console.WriteLine("_\nstart\n_");
+            client.HowMoney();
+            Console.WriteLine("_\nadd 10 money\n_");
+            account.Plus(10);
+            client.HowMoney();
+            Console.WriteLine("_\nminus 1 money\n_");
+            account.Minus(1);
+            client.HowMoney();
+
+        }
+    }
+}
